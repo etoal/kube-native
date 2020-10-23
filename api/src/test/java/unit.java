14cb6f3789e5a0cd56889df121d57a211120334f
etoal <eric.toal@gmail.com>
2020-10-23 20:19:03 +0000

    unit test changes

--- api/src/test/java/unit.java
+++ api/src/test/java/unit.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.docker.demo.model.Product; 
+import static org.junit.Assert.fail;
 
 class apiUnitTests {
  
@@ -8,5 +9,7 @@ class apiUnitTests {
     void justAnExample() {
     	Product testProd = new Product((long)9999, "testWidget", 19.95);
         assertEquals(testProd.getProductId(), 9999);
+        assertEquals(9, 4 + 5);
+        fail("ET - making the test fail");
     }
 }
